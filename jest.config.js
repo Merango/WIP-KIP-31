568f506a8f3349571928c9c457a8a29cba4017a9
Merango <Merango@users.noreply.github.com>
2025-05-13 18:32:07 +0000

    Create dedicated Jest configuration file

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('jest').Config} */
+export default {
+  testEnvironment: 'node',
+  verbose: true,
+  collectCoverage: true,
+  extensionsToTreatAsEsm: ['.js'],
+  transform: {},
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  }
+};
