3d98110e0b572f784ff8052ce1c5afb122af80ac
Merango <Merango@users.noreply.github.com>
2025-05-14 12:01:56 +0000

    Update Jest configuration for TypeScript support

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
-
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
-
-  // A list of paths to directories that Jest should use to search for files
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-
-  // The test environment that will be used for testing
+  preset: 'ts-jest',
   testEnvironment: 'node',
-
-  // A map from regular expressions to paths to transformers
+  roots: ['<rootDir>/tests'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-
-  // An array of regexp pattern strings that are matched against all test paths
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-
-  // Module file extensions for importing
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
 };
